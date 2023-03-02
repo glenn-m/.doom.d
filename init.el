ef6dc7bd725c1b2c0def8336bedc98c726e758d3
Glenn McDonald <2371316+glenn-m@users.noreply.github.com>
2023-03-02 09:57:36 -0500

    Rust & Latex support

--- init.el
+++ init.el
@@ -118,7 +118,7 @@
        ;;coq                  ; proofs-as-programs
        ;;crystal              ; ruby at the speed of c
        ;;csharp               ; unity, .NET, and mono shenanigans
-       ;;data                 ; config/data formats
+       data                   ; config/data formats
        ;;(dart +flutter)      ; paint ui and not much else
        elixir                 ; erlang done right
        ;;elm                  ; care for a cup of TEA?
@@ -139,7 +139,7 @@
        javascript             ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia                ; a better, faster MATLAB
        ;;kotlin               ; a better, slicker Java(Script)
-       ;;latex                ; writing papers in Emacs has never been so fun
+       latex                  ; writing papers in Emacs has never been so fun
        ;;lean                 ; for folks with too much to prove
        ;;ledger               ; be audit you can be
        ;;lua                  ; one-based indices? one-based indices
@@ -158,7 +158,7 @@
        ;;rest                 ; Emacs as a REST client
        ;;rst                  ; ReST in peace
        ;;(ruby +rails)        ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust                 ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       rust                   ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala                ; java, but good
        ;;(scheme +guile)      ; a fully conniving family of lisps
        sh                     ; she sells {ba,z,fi}sh shells on the C xor
