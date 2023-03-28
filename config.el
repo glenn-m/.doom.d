7f582a9cf99c6078a1fe0e6b2bd4271f9db33043
Glenn McDonald <2371316+glenn-m@users.noreply.github.com>
2023-03-27 21:41:12 -0400

    bug fix

--- config.el
+++ config.el
@@ -10,8 +10,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name 'gm/name
-      user-mail-address 'gm/email)
+(setq user-full-name gm/user
+      user-mail-address gm/email)
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
 ;;
